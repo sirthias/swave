0f6a8ee1b9b50611ca37b45e29d09058aa7f0dc1
Mathias <mathias@swave.io>
2016-11-16 17:04:39 +0100

    =cor refactor `Coupling`

--- core/src/main/scala/swave/core/Coupling.scala
+++ core/src/main/scala/swave/core/Coupling.scala
@@ -7,18 +7,24 @@
 package swave.core
 
 import swave.core.impl.stages.inout.CouplingStage
+import swave.core.util.AnyRefExtractor.Extraction
 
-sealed abstract class Coupling[T] {
-  def in: Drain[T, Unit]
-  def out: Spout[T]
+/**
+  * A simple streaming component with one input, one output port and no internal transformation logic.
+  *
+  * It is typically used to manually connect two ports of a stream graph that cannot
+  * be otherwise connected via the streaming DSL, e.g. for creating cycles.
+  */
+final class Coupling[T] private {
+  private[this] val stage = new CouplingStage
+
+  val in: Drain[T, Unit] = Drain[T](stage)
+  val out: Spout[T]      = new Spout[T](stage)
 }
 
 object Coupling {
+  def apply[T]: Coupling[T] = new Coupling[T]
 
-  def apply[T]: Coupling[T] =
-    new Coupling[T] {
-      private[this] val pipe = new CouplingStage
-      val in                 = Drain[T](pipe)
-      val out                = new Spout[T](pipe)
-    }
+  def unapply[T](value: Coupling[T]): Extraction[(Drain[T, Unit], Spout[T])] =
+    new Extraction(value.in -> value.out)
 }
